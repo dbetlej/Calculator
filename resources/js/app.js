6d37d920043189b4fd5d37a8911a8c6111be38b2
Dominik <dominik.filip.betlej@gmail.com>
2022-04-04 21:26:37 +0200

    add: breeze & inertia & react | fix: user

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,73 +1,18 @@
 require('./bootstrap');
 
-$.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
+import React from 'react';
+import { render } from 'react-dom';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { InertiaProgress } from '@inertiajs/progress';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        return render(<App {...props} />, el);
+    },
 });
 
-$('#add_button').click(function(){
-   $.ajax({
-      method: 'post',
-      url: '/add_movies',
-      dataType: 'json',
-      data: $('#add_movies').serialize()
-   }).always(function(res){
-        console.log(res.status);
-        if(res.status == 1 || res.status == 2){
-        // return false
-        $('#info_popup').html(res.msg).fadeIn();
-        setTimeout(function(){
-           $('#info_popup').fadeOut();
-        },5000);
-       } else {
-        // return pass
-        $('#info_popup').html(res.msg).fadeIn();
-        setTimeout(function(){
-           $('#info_popup').fadeOut(); 
-        },5000);
-       }
-   });
-});
-
-$('#add_movies_modal').click(function(){
-    $('#add_movies').fadeToggle();
-});
-
-$('#edit_movies_modal').click(function(){
-    $('#edit_movie').fadeToggle();
-});
-
-$('.close').click(function(){
-    $('#add_movies').fadeOut();
-})
-
-
-//  Vanilla s
-document.addEventListener('click', function(event){
-    if(!event.target.matches('.edit_button')) return;
-    event.preventDefault();
-    
-    var favourite = 0;
-    var watched = 0;
-    if(document.getElementById('favourite').checked){
-        favourite = 1;
-    }
-    if(document.getElementById('watched').checked){
-        watched = 1;
-    }
-    var fd = new FormData();
-    fd.append('title', document.getElementById('title').value);
-    fd.append('url', document.getElementById('url').value);
-    fd.append('favourite', favourite);
-    fd.append('watched', watched);
-    fd.append('_token', $('meta[name="csrf-token"]').attr('content'));
-    var movieId = document.getElementById('movieId').value;
-    var req = new XMLHttpRequest();
-    req.open('POST', '/movie/'+movieId, true);
-    req.send(fd);
-    req.onload = function(){
-        console.log(req.response);
-    }
-}, false);
-//  Vanilla e
+InertiaProgress.init({ color: '#4B5563' });
